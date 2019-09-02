7237d35be5e1466d3ce674a337637d921e72fcf0
boguslaw.ziolo <Tuchow123>
2019-09-02 13:34:32 +0200

    kwargs and args

--- kwargsy.py
+++ kwargsy.py
@@ -5,3 +5,36 @@ def tag(name, **attributes):
 slownik = {'owoc': "jablko ", 'warzywo': 'pietrucha'}
 
 tag("warzywniak", owoc = "jablko ", warzywo = 'pietrucha')
+
+def print_args(arg1, arg2, *args):
+    print(arg1)
+    print(arg2)
+    print(args)
+
+t = (1, 2, 3, 4)
+# *t to wtedy on sobie sam da arg1 i 2 a z reszty zrobi tuple (extended Call Syntax)
+print_args(*t)
+
+def color(red, green, blue, **kwargs):
+    print('r =', red)
+    print('g =', green)
+    print('b =', blue)
+    print(kwargs)
+
+k = {'red':21, 'green':68, 'blue':120, 'alpha':52}
+
+print("extended call function kwargs example!")
+color(**k)
+
+slownik = dict(red=21, blue=44)
+print(slownik)
+
+#kwargsy uzywane do przekierownia
+def f(*args, **kwargs):
+    print("args =", args)
+    print("kwargs =", kwargs)
+
+def trace(functition_def, *args, **kwargs):
+    f(*args, **kwargs)
+
+trace(f, 'ff', 'gg', base = 16, name = 'IBM')
