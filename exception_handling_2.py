7182c3447bd69979df0e3a98e16f2db92ad1a836
boguslaw.ziolo <Tuchow123>
2019-09-10 13:07:54 +0200

    exception basic example

---
+++ exception_handling_2.py
@@ -0,0 +1,20 @@
+def median(iterable):
+    items = sorted(iterable)
+    if len(items) == 0:
+        raise ValueError("median() arg is an emtpy sequence")
+    median_index = (len(items) - 1) // 2
+    if len(items) % 2 !=0:
+        return items[median_index]
+    return (items[median_index] + items[median_index + 1]) / 2
+
+def main():
+    try:
+        #empty list cause handeled error
+        median([])
+    except ValueError as e:
+        print("Payload:" , e.args)
+
+
+
+if __name__ == '__main__':
+    main()
